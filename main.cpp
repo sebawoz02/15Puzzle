b180d05f5af58a1ab28831fa1853c4a582c867ba
sebawoz02 <268491@student.pwr.edu.pl>
2023-04-21 11:37:17 +0200

    Pierwsza wersja z heurystyką manhattan

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 #include <iostream>
 #include <bits/stdc++.h>
+#include <cmath>
 
 #define BITS_PER_TILE  4
-#define MASK 0x00000000fULL
+#define MASK 0x00000000000000000fULL
 #define Board unsigned long long
 
 using namespace std;
@@ -21,7 +22,7 @@ public:
     State* parent;
 
     void setTile(int idx, int value);
-    char getTile(int idx) const;
+    int getTile(int idx) const;
 
     State(Board board, unsigned int g, unsigned int f, char parentMove, State *parent){
         this->board = board;
@@ -32,17 +33,16 @@ public:
     }
 };
 
-auto cmp = [](State* left, State* right){return left->f_cost < right->f_cost;};
 
 // Returns board element under given index
-char State::getTile(int idx) const {
-    return (this->board >> (idx*BITS_PER_TILE)) & MASK;
+int State::getTile(int idx) const {
+    return (this->board >> ((boardWidth*boardWidth - 1 - idx)*BITS_PER_TILE)) & MASK;
 }
 
-// Sets new value of the board element under given index
+// Sets the new value of the board element at the given index
 void State::setTile(int idx, int value){
-    this->board &= ~(MASK << (idx * BITS_PER_TILE));
-    this->board |= static_cast<Board>(value) << (idx * BITS_PER_TILE);
+    this->board &= ~(MASK << ((boardWidth*boardWidth - 1 - idx) * BITS_PER_TILE));
+    this->board |= static_cast<Board>(value) << ((boardWidth*boardWidth - 1 - idx) * BITS_PER_TILE);
 }
 
 // Function calculates count of inversions in given state.
@@ -50,7 +50,7 @@ int getInversionCount(State *state){
     int inv_count = 0;
     for(int i = 0; i<boardWidth*boardWidth; i++){
         for(int j = i+1; j<=boardWidth*boardWidth; j++){
-            if(state->getTile(j) && state->getTile(i) && state->getTile(i) > state->getTile(j))
+            if(state->getTile(j)!=0 && state->getTile(i)!=0 && state->getTile(i) > state->getTile(j))
                 inv_count++;
         }
     }
@@ -58,7 +58,7 @@ int getInversionCount(State *state){
 }
 
 // Finds index of blank tile.
-char getBlankPos(State *state){
+int getBlankPos(State *state){
     for(int i = boardWidth*boardWidth - 1; i>=0 ; --i){
         if(state->getTile(i) == 0) return i;
     }
@@ -70,166 +70,182 @@ bool isSolvable(State *state){
     int invCount = getInversionCount(state);
     if(boardWidth & 1)return !(invCount & 1);
     else {
-        char pos = getBlankPos(state);
+        int pos = getBlankPos(state);
         if(pos & 1) return !(invCount & 1);
         else return invCount & 1;
     }
 }
 
 
-unsigned char manhattanHeuristic(Board board){
-    for(size_t i = 0; i<boardWidth*boardWidth; i++){
-        return 0;
+unsigned char manhattanHeuristic(State* board){
+    unsigned int f_cost = 0;
+    for(int i = 0; i<boardWidth*boardWidth; i++){
+        int dest = board->getTile(i);
+        if(dest!=0){ f_cost += (int) abs(floor(i/boardWidth) - floor((dest-1)/boardWidth)) + abs((i%boardWidth) - ((dest-1)%boardWidth));}
     }
-    return 0;
+    return f_cost + board->g_cost;
 }
 
 // prints the solution
-void solutionFound(State* state){
-
+void solutionFound(State* state, long visited){
+    cout << "Solution found! Visited states: " << visited << endl;
+    State* solution = state;
+    unsigned int moves = 0;
+    while(solution->parent != nullptr){
+        moves++;
+        solution = solution->parent;
+    }
+    cout << "Moves: " << moves << endl;
 }
+
+struct Hash{
+    size_t operator()(const State* state) const{
+        return hash<Board>()(state->board);
+    }
+};
+
+struct Equal{
+    bool operator()(const State* obj1, const State* obj2) const{
+        return obj1->board == obj2->board;
+    }
+};
+
+
 
 // A* algorithm to find the best solution from initialState to goal
 // returns -1 if not solvable, else 0
 int AstarSearch(State* initialState, Board goal){
-    if(!isSolvable(initialState)) return -1;
+    if(!isSolvable(initialState)) {
+        cout << "Not Solvable!" << endl;
+        return -1;
+    }
+    cout << "Solving..." << endl;
+    auto cmp = [](State* left, State* right){return left->f_cost < right->f_cost;};
 
+    long visited = 0;
     // open list for states to be checked
-    multiset<State*, decltype(cmp)> openList(cmp);;
+    priority_queue<State*, vector<State*>,decltype(cmp)> openList(cmp);
     // checked states
-    unordered_set<State*> closedList;
+
+    unordered_set<const State*, Hash, Equal> closedList;
     // set iterator
-    openList.insert(initialState);
+    openList.push(initialState);
+    closedList.insert(initialState);
     while (!openList.empty()){
         // state with the lowest f_cost
-        State* q = *openList.begin();
-        openList.erase(q);
+        State* q = openList.top();
+        openList.pop();
         closedList.insert(q);
-
+        visited++;
         // generate successors
-        char blankPos = getBlankPos(q);
+        int blankPos = getBlankPos(q);
+        if(blankPos == -1){
+            return -1;
+        }
         //-------------------------- 1 - move blank spot higher --------------------------
-        if(blankPos >= boardWidth){
+        if(blankPos >= boardWidth && q->parentMove != 'd'){
             auto* topSuccessor = new State(q->board, q->g_cost + 1, 0, 't', q);
-            char val = topSuccessor->getTile(blankPos-boardWidth);
+            int val = topSuccessor->getTile(blankPos-boardWidth);
             topSuccessor->setTile(blankPos, val);
             topSuccessor->setTile(blankPos-boardWidth, 0x0000);
             if(topSuccessor->board == goal){
-                solutionFound(topSuccessor);
+                solutionFound(topSuccessor, visited);
                 return 0;
             }
-            topSuccessor->f_cost = manhattanHeuristic(topSuccessor->board) + topSuccessor->g_cost;
+            topSuccessor->f_cost = manhattanHeuristic(topSuccessor);
 
-            for(auto itr : openList){
-                if(itr->board == topSuccessor->board){
-                    if(itr->f_cost>topSuccessor->f_cost){
-                        for(auto itr2 : closedList){
-                            if(itr2->board == topSuccessor->board){
-                                if(itr->f_cost>topSuccessor->f_cost){
-                                    openList.insert(topSuccessor);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                }
+            auto search = closedList.find(topSuccessor);
+            if(search == closedList.end()){
+                closedList.insert(topSuccessor);
+                openList.push(topSuccessor);
             }
-
+            else if(topSuccessor->f_cost < (*search)->f_cost){
+                openList.push(topSuccessor);
+                closedList.insert(topSuccessor);
+                closedList.erase(search);
+            } else delete topSuccessor;
 
         }
         // -------------------------- 2 - move blank spot lower --------------------------
-        if(blankPos <  (boardWidth*boardWidth)-boardWidth ){
+        if(blankPos < (boardWidth*boardWidth-boardWidth) && q->parentMove!='t'){
             auto* lowSuccessor = new State(q->board, q->g_cost + 1, 0, 'd', q);
-            char val = lowSuccessor->getTile(blankPos+boardWidth);
+            int val = lowSuccessor->getTile(blankPos+boardWidth);
             lowSuccessor->setTile(blankPos, val);
             lowSuccessor->setTile(blankPos+boardWidth, 0x0000);
             if(lowSuccessor->board == goal){
-                solutionFound(lowSuccessor);
+                solutionFound(lowSuccessor, visited);
                 return 0;
             }
-            lowSuccessor->f_cost = manhattanHeuristic(lowSuccessor->board) + lowSuccessor->g_cost;
+            lowSuccessor->f_cost = manhattanHeuristic(lowSuccessor);
 
-            for(auto itr : openList){
-                if(itr->board ==  lowSuccessor->board){
-                    if(itr->f_cost> lowSuccessor->f_cost){
-                        for(auto itr2 : closedList){
-                            if(itr2->board ==  lowSuccessor->board){
-                                if(itr->f_cost> lowSuccessor->f_cost){
-                                    openList.insert( lowSuccessor);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                }
+            auto search = closedList.find(lowSuccessor);
+            if(search == closedList.end()){
+                closedList.insert(lowSuccessor);
+                openList.push(lowSuccessor);
             }
+            else if(lowSuccessor->f_cost < (*search)->f_cost){
+                openList.push(lowSuccessor);
+                closedList.insert(lowSuccessor);
+                closedList.erase(search);
+            } else delete lowSuccessor;
+
         }
         // -------------------------- 3 - move blank spot left --------------------------
-        if(blankPos % boardWidth != 0){
+        if(blankPos % boardWidth != 0 && q->parentMove != 'p'){
             auto* leftSuccessor = new State(q->board, q->g_cost + 1, 0, 'l', q);
-            char val = leftSuccessor->getTile(blankPos-1);
+            int val = leftSuccessor->getTile(blankPos-1);
             leftSuccessor->setTile(blankPos, val);
             leftSuccessor->setTile(blankPos-1, 0x0000);
             if(leftSuccessor->board == goal){
-                solutionFound(leftSuccessor);
+                solutionFound(leftSuccessor, visited);
                 return 0;
             }
-            leftSuccessor->f_cost = manhattanHeuristic(leftSuccessor->board) + leftSuccessor->g_cost;
+            leftSuccessor->f_cost = manhattanHeuristic(leftSuccessor);
 
-            for(auto itr : openList){
-                if(itr->board == leftSuccessor->board){
-                    if(itr->f_cost>leftSuccessor->f_cost){
-                        for(auto itr2 : closedList){
-                            if(itr2->board == leftSuccessor->board){
-                                if(itr->f_cost>leftSuccessor->f_cost){
-                                    openList.insert(leftSuccessor);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                }
+            auto search = closedList.find(leftSuccessor);
+            if(search == closedList.end()){
+                closedList.insert(leftSuccessor);
+                openList.push(leftSuccessor);
             }
+            else if(leftSuccessor->f_cost < (*search)->f_cost){
+                openList.push(leftSuccessor);
+                closedList.insert(leftSuccessor);
+                closedList.erase(search);
+            } else delete leftSuccessor;
+
         }
         // -------------------------- 4 - move blank spot right --------------------------
-        if(blankPos % boardWidth != boardWidth-1){
+        if(blankPos % boardWidth < boardWidth-1 && q->parentMove != 'l'){
             auto* rightSuccessor = new State(q->board, q->g_cost + 1, 0, 'p', q);
-            char val = rightSuccessor->getTile(blankPos+1);
+            int val = rightSuccessor->getTile(blankPos+1);
             rightSuccessor->setTile(blankPos, val);
             rightSuccessor->setTile(blankPos+1, 0x0000);
             if(rightSuccessor->board == goal){
-                solutionFound(rightSuccessor);
+                solutionFound(rightSuccessor, visited);
                 return 0;
             }
-            rightSuccessor->f_cost = manhattanHeuristic(rightSuccessor->board) + rightSuccessor->g_cost;
+            rightSuccessor->f_cost = manhattanHeuristic(rightSuccessor);
 
-            for(auto itr : openList){
-                if(itr->board == rightSuccessor->board){
-                    if(itr->f_cost>rightSuccessor->f_cost){
-                        for(auto itr2 : closedList){
-                            if(itr2->board == rightSuccessor->board){
-                                if(itr->f_cost>rightSuccessor->f_cost){
-                                    openList.insert(rightSuccessor);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                }
+            auto search = closedList.find(rightSuccessor);
+            if(search == closedList.end()){
+                closedList.insert(rightSuccessor);
+                openList.push(rightSuccessor);
             }
+            else if(rightSuccessor->f_cost < (*search)->f_cost){
+                openList.push(rightSuccessor);
+                closedList.insert(rightSuccessor);
+                closedList.erase(search);
+            } else delete rightSuccessor;
         }
     }
     return -1;
 }
 
 
-int main() {
-    Board goal = 0x123456780ULL;
-    Board randomState = 0x125463780ULL;
-    auto* state = new State(randomState, 0, 0, '\0', nullptr);
 
+int main() {
+    Board goalBoard = 0x123456780ULL;
+    Board randomState = 0x1235468780ULL;
+    auto* state = new State(randomState, 0, 0, '\0', nullptr);
+    return AstarSearch(state, goalBoard);
 }
